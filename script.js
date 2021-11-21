d2d1794e68d588842ad9203822f98e76314b8e94
Elijah Busick <elibusick11@gmail.com>
2021-11-21 11:20:25 -0500

    added variables

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // Assignment code here
-
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_-+=";
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("password");
+var length = document.querySelector("length");
+var numbers = document.querySelector("numbers");
+var symbols = document.querySelector("symbols")
+var generateBtn = document.querySelector("generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("password");
 
   passwordText.value = password;
 
