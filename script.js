0d5aa6020171d73148dbfb39bf96e6a0cd9ca908
Elijah Busick <elibusick11@gmail.com>
2021-11-21 17:32:53 -0500

    added var and if statements for password length

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // Assignment code here
-var lowercasealpha = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-var uppercassalpha = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+var lowercasealpha = { a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+var uppercassalpha = { A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
 var numbers = {123456789:any};
-var specilchar = [!, "", #, $, %, &, '', (, ),  *, +, =, -, ., /, :, ;, <, >, ?, @, \, ^, _, |, ~];
+var specilchar = ['!', '#', $, , ':' , ';', '<', '>', '?', '@', '^', '_' , '|', '~'];
+
+function passWordOptions() {
+  // variable to store length for user input
+  var length = parseInt(
+    prompt('How many characters whould you like to have?')
+  );
+  
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+
+  if (length > 128){
+    alert('Password must be less than 128 characters!');
+    return;
+  }
+}
 
 function generatePassword() {
   
